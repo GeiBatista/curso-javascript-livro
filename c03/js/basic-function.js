2fa16716e6b163619272326ecc21676fb2cc4ac4
GeiBatista <geibatista@hotmail.com>
2021-02-01 23:26:12 -0300

    Projeto criado

---
+++ c03/js/basic-function.js
@@ -0,0 +1,6 @@
+var msg = 'inscreva-se para receber a nossa newsletter com 10% de desconto!';
+function updateMessage(){
+	var el = document.getElementById('message');
+	el.textContent = msg;
+}
+updateMessage();
